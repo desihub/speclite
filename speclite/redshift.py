057953f89ef2b98d3ecfa96c97a073567cbdff5a
David Kirkby <dkirkby@uci.edu>
2015-07-22 18:05:18 -0700

    Implement and test z broadcasting

--- speclite/redshift.py
+++ speclite/redshift.py
@@ -4,54 +4,90 @@ import numpy as np
 def transform(z, to_rest_frame=False, data_in=None, data_out=None,
               wavelength='wavelength', flux='flux'):
     """
-    Apply a redshift to a spectrum.
+    Apply a redshift transform to a spectrum.
+
+    The input spectrum (or spectra) must have at least wavelength and flux values,
+    which are transformed according to::
+
+        wavelength_out = wavelength_in * (1 + z)
+        flux_out = flux_in / (1 + z)
+
+    or, if ``to_rest_frame`` is True::
+
+        wavelength_out = wavelength_in / (1 + z)
+        flux_out = flux_in * (1 + z)
+
+    The usual `numpy broadcasting rules <http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html>`_ apply so that the
+    same redshift can be applied to multiple spectra, or different redshifts can be
+    applied to the same spectrum with appropriate input shapes.
 
     Parameters
     ----------
-    z: float
-        The redshift to apply. Must be >= 0.
+    z: float or numpy.ndarray
+        The redshift(s) to apply, which can either be a single numerical value >= 0
+        or else a numpy array of values >= 0.
     to_rest_frame: bool
-        Divide wavelength and flux by 1+z instead of multiplying by 1+z.
+        Divide wavelengths by 1+z instead of multiplying by 1+z.
+    data_in: numpy.ndarray
+        Structured numpy array containing input spectrum data to transform.
+    data_out: numpy.ndarray
+        Structured numpy array where output spectrum data should be written.
+    wavelength: string or numpy array
+        Either the field name in data_in containing wavelengths or else a numpy
+        array of wavelength values.
+    flux: string or numpy.ndarray
+        Either the field name in data_in containing fluxes or else a numpy
+        array of flux values.
 
     Returns
     -------
     result: numpy.ndarray
-        Array of spectral data with the redshift applied.
+        Array of spectrum data with the redshift applied. Equal to data_out when
+        set, otherwise a new array is allocated.
     """
 
     if not isinstance(z, np.ndarray):
-        z = np.array([z], dtype=float)
+        z = np.float(z)
     if np.any(z < 0):
         raise ValueError('Found invalid z < 0.')
 
-    if data_in is not None and isinstance(wavelength, basestring):
-        wavelength_name = wavelength
-        wavelength = data_in[wavelength_name]
-    if data_in is not None and isinstance(flux, basestring):
-        flux_name = flux
-        flux = data_in[flux_name]
+    if data_in is not None or data_out is not None:
+        if not isinstance(wavelength, basestring):
+            raise ValueError('Invalid wavelength field name: {0}.'.format(wavelength))
+        if not isinstance(flux, basestring):
+            raise ValueError('Invalid flux field name: {0}.'.format(flux))
 
-    if wavelength.shape != flux.shape:
-        raise ValueError('wavelength and flux arrays have different shapes: {0}, {1}'
-            .format(wavelength.shape, flux.shape))
+    if data_in is None:
+        wavelength_in = wavelength
+        wavelength = 'wavelength'
+        flux_in = flux
+        flux = 'flux'
+    else:
+        wavelength_in = data_in[wavelength]
+        flux_in = data_in[flux]
+    if wavelength_in.shape != flux_in.shape:
+        raise ValueError('Input wavelength and flux arrays have different shapes: {0}, {1}'
+            .format(wavelength_in.shape, flux_in.shape))
 
     if data_out is None:
         if data_in is None:
-            dtype = [('wavelength', wavelength.dtype), ('flux', flux.dtype)]
-            data_out = np.empty(shape=wavelength.shape, dtype=dtype)
-            wavelength_name = 'wavelength'
-            flux_name = 'flux'
+            dtype = [(wavelength, wavelength_in.dtype), (flux, flux_in.dtype)]
+            data_out = np.empty(shape=wavelength_in.shape, dtype=dtype)
+        elif isinstance(z, np.ndarray):
+            data_out = np.empty(shape=np.broadcast(data_in, z).shape, dtype=data_in.dtype)
+            data_out[...] = data_in
         else:
             data_out = np.copy(data_in)
-    wavelength_out = data_out[wavelength_name]
-    flux_out = data_out[flux_name]
+
+    wavelength_out = data_out[wavelength]
+    flux_out = data_out[flux]
 
     if to_rest_frame:
         zfactor = 1 / (1 + z)
     else:
         zfactor = 1 + z
 
-    wavelength_out[:] = wavelength * zfactor
-    flux_out[:] = flux * zfactor
+    wavelength_out[:] = wavelength_in * zfactor
+    flux_out[:] = flux_in * zfactor
 
     return data_out
--- speclite/tests/test_redshift.py
+++ speclite/tests/test_redshift.py
@@ -12,6 +12,27 @@ def test_negative_z():
         transform(z=np.array([0, -1, 2]), wavelength=wavelength, flux=flux)
 
 
+def test_invalid_field_name():
+    data_in = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    with pytest.raises(ValueError):
+        transform(z=0, data_in=data_in, wavelength=np.arange(10))
+    with pytest.raises(ValueError):
+        transform(z=0, data_in=data_in, wavelength=123)
+
+
+def test_missing_input_field():
+    data_in = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    with pytest.raises(ValueError):
+        transform(z=0, data_in=data_in, wavelength='wavelength')
+
+
+def test_missing_output_field():
+    data_in = np.empty((10,), dtype=[('wavelength', np.float32), ('flux', np.float64)])
+    data_out = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    with pytest.raises(ValueError):
+        transform(z=0, data_in=data_in, data_out=data_out)
+
+
 def test_different_shapes():
     wavelength = np.arange(10)
     flux = np.zeros(shape=(11,))
@@ -22,25 +43,25 @@ def test_different_shapes():
 def test_separate_arrays():
     wavelength = np.arange(10, dtype=np.float32)
     flux = np.zeros(shape=(10,), dtype=np.float64)
-    out = transform(z=0, wavelength=wavelength, flux=flux)
-    assert out.shape == (10,), 'Unexpected output array shape.'
-    assert out.dtype == [('wavelength', np.float32), ('flux', np.float64)],\
+    result = transform(z=0, wavelength=wavelength, flux=flux)
+    assert result.shape == (10,), 'Unexpected output array shape.'
+    assert result.dtype == [('wavelength', np.float32), ('flux', np.float64)],\
         'Unexpected output array dtype.'
-    assert np.allclose(wavelength, out['wavelength']), 'Invalid wavelength result.'
-    assert np.allclose(flux, out['flux']), 'Invalid flux result.'
+    assert np.allclose(wavelength, result['wavelength']), 'Invalid wavelength result.'
+    assert np.allclose(flux, result['flux']), 'Invalid flux result.'
 
 
 def test_data_in():
     data_in = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
     data_in['wlen'] = np.arange(10)
     data_in['flux'] = 0.
-    data_out = transform(z=0, data_in=data_in, wavelength='wlen')
-    assert data_out.shape == (10,), 'Unexpected output array shape.'
-    assert data_out.dtype == [('wlen', np.float32), ('flux', np.float64)],\
+    result = transform(z=0, data_in=data_in, wavelength='wlen')
+    assert result.shape == (10,), 'Unexpected output array shape.'
+    assert result.dtype == [('wlen', np.float32), ('flux', np.float64)],\
         'Unexpected output array dtype.'
-    assert np.allclose(data_in['wlen'], data_out['wlen']), 'Invalid wavelength result.'
-    assert np.allclose(data_in['flux'], data_out['flux']), 'Invalid flux result.'
-    assert data_out.base is None, 'Result does not own its memory.'
+    assert np.allclose(data_in['wlen'], result['wlen']), 'Invalid wavelength result.'
+    assert np.allclose(data_in['flux'], result['flux']), 'Invalid flux result.'
+    assert result.base is None, 'Result does not own its memory.'
 
 
 def test_data_in_different_out():
@@ -70,9 +91,32 @@ def test_data_in_same_out():
 def test_round_trip():
     wavelength = np.arange(10)
     flux = np.arange(10)
-    out = transform(z=1, to_rest_frame=False, wavelength=wavelength, flux=flux)
-    out = transform(z=1, to_rest_frame=True, data_in=out)
-    assert np.allclose(wavelength, out['wavelength']),\
+    result = transform(z=1, to_rest_frame=False, wavelength=wavelength, flux=flux)
+    result = transform(z=1, to_rest_frame=True, data_in=result)
+    assert np.allclose(wavelength, result['wavelength']),\
         'Wavelength arrays not equal after round trip.'
-    assert np.allclose(flux, out['flux']),\
+    assert np.allclose(flux, result['flux']),\
         'Flux arrays not equal after round trip.'
+
+
+def test_multiple_spectra():
+    data_in = np.empty((2, 10), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    result = transform(z=0, data_in=data_in, wavelength='wlen')
+    assert np.allclose(data_in['wlen'], result['wlen']), 'Invalid wavelength result.'
+    assert np.allclose(data_in['flux'], result['flux']), 'Invalid flux result.'
+
+
+def test_broadcast_error():
+    data_in = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    z = np.zeros(shape=(2,))
+    with pytest.raises(ValueError):
+        transform(z=z, data_in=data_in, wavelength='wlen')
+
+
+def test_multiple_z():
+    data_in = np.empty((10,), dtype=[('wlen', np.float32), ('flux', np.float64)])
+    z = np.zeros(shape=(2, 3, 1))
+    result = transform(z=z, data_in=data_in, wavelength='wlen')
+    assert result.shape == (2, 3, 10), 'Invalid output shape.'
+    assert np.allclose(data_in['wlen'], result['wlen'][1, 0]), 'Invalid wavelength result.'
+    assert np.allclose(data_in['flux'], result['flux'][0, 2]), 'Invalid flux result.'
