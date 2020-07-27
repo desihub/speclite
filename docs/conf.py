a30c378b1f1e187668b76245ad9a002ab6adf7ba
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-07-27 13:46:22 -0700

    fix doc configuration

--- docs/conf.py
+++ docs/conf.py
@@ -84,8 +84,8 @@ copyright = '{0}, {1}'.format(
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 
-__import__(setup_cfg['name'])
-package = sys.modules[setup_cfg['name']]
+__import__(setup_cfg['package_name'])
+package = sys.modules[setup_cfg['package_name']]
 
 # The short X.Y version.
 version = package.__version__.split('-', 1)[0]
@@ -164,7 +164,7 @@ man_pages = [('index', project.lower(), project + u' Documentation',
 if eval(setup_cfg.get('edit_on_github')):
     extensions += ['sphinx_astropy.ext.edit_on_github']
 
-    versionmod = __import__(setup_cfg['name'] + '.version')
+    versionmod = __import__(setup_cfg['package_name'] + '.version')
     edit_on_github_project = setup_cfg['github_project']
     if versionmod.version.release:
         edit_on_github_branch = "v" + versionmod.version.version
