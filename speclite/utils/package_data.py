8e61f14248771114ca9beba84c53bc27b1a34825
grburgess <jmichaelburgess@gmail.com>
2021-08-17 09:32:17 +0200

    fix another typo

--- speclite/utils/package_data.py
+++ speclite/utils/package_data.py
@@ -1,12 +1,12 @@
 import os
-import pkg_resources
 
+import pkg_resources
 
 # TODO: should make these Path objects
 
 def get_path_of_data_file(data_file) -> str:
     file_path = pkg_resources.resource_filename(
-        "speclite", os.path.join("data", f"{data_file}")
+        "speclite", os.path.join("data", f"{data_file}"))
 
     return file_path
 
