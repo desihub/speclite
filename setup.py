c9bfbacb225cf98c0c047e2365cf1293bea8b002
Benjamin Alan Weaver <benjamin.weaver@noirlab.edu>
2026-01-16 15:39:15 -0700

    do not add git information to version

--- setup.py
+++ setup.py
@@ -74,4 +74,5 @@ except Exception:
 """.lstrip()
 
 setup(use_scm_version={'write_to': os.path.join('speclite', 'version.py'),
-                       'write_to_template': VERSION_TEMPLATE})
+                       'write_to_template': VERSION_TEMPLATE,
+                       'local_scheme': 'no-local-version'})
